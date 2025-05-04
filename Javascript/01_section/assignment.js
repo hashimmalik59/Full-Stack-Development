cdaa54deeb9c0e4326e8e478999b3e271fce42be
hashimmalik59 <hm363050@gmail.com>
2025-05-04 12:21:21 -0700

    Assignment of Basic Operators

--- Javascript/01_section/assignment.js
+++ Javascript/01_section/assignment.js
@@ -7,7 +7,7 @@
 
 const myCountry = "Pakistan";
 const myContinent = "Asia";
-let myCountryPopulation = "250M";
+let myCountryPopulation = "250000000";
 
 console.log(myCountry, myContinent, myCountryPopulation);
 
@@ -28,3 +28,37 @@ console.log(isIsland, myCountryPopulation, myCountry, myCountryLanguage);
 // Try to change one of the changed variables now, and observe what happens.
 
 myCountryLanguage = "Urdu";
+
+
+// Basic Operators
+// If your country split in half, and each half would contain half the population, then how many people would live in each half?
+// Increase the population of your country by 1 and log the result to the console.
+// Finland has a population of 6 million. Does your country have more people than Finland?
+// The average population of a country is 33 million people. Does you country have less people than the average country?
+// Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
+
+
+myCountryPopulation /= 2;
+console.log(myCountryPopulation);
+
+myCountryPopulation /= 2;
+console.log(myCountryPopulation);
+
+let newPopulaiton = 25000000000 + 1;
+console.log(newPopulaiton);
+
+let populationOfFinland = 6000000;
+
+console.log(newPopulaiton > populationOfFinland);
+
+console.log(myCountryPopulation + " is less then our 33M population");
+
+let europeanCountry = "Portugal";
+let continentOfPortugal = "Europe"
+let populationOfPortugal = 11;
+let portugalPopulationInMillion = "M";
+let laguageOfPortugal = "portuguese"
+
+console.log(`${europeanCountry} is in ${continentOfPortugal}, and it's ${populationOfPortugal}${portugalPopulationInMillion} people speak ${laguageOfPortugal}`);
+
+
