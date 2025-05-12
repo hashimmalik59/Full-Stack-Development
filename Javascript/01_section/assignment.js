165a2662c676245e2bfa5fda78358c3afcc710aa
hashimmalik59 <hm363050@gmail.com>
2025-05-12 22:27:42 +0500

    Strings and Template Literals assignment

--- Javascript/01_section/assignment.js
+++ Javascript/01_section/assignment.js
@@ -11,6 +11,7 @@ let myCountryPopulation = "250000000";
 
 console.log(myCountry, myContinent, myCountryPopulation);
 
+//////////////////////////////////////////
 
 // Data Types tasks
 
@@ -21,6 +22,7 @@ let isIsland = true;
 let myCountryLanguage;
 console.log(isIsland, myCountryPopulation, myCountry, myCountryLanguage);
 
+//////////////////////////////////////////
 
 // let, const and var
 // Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
@@ -29,6 +31,7 @@ console.log(isIsland, myCountryPopulation, myCountry, myCountryLanguage);
 
 myCountryLanguage = "Urdu";
 
+//////////////////////////////////////////
 
 // Basic Operators
 // If your country split in half, and each half would contain half the population, then how many people would live in each half?
@@ -36,7 +39,6 @@ myCountryLanguage = "Urdu";
 // Finland has a population of 6 million. Does your country have more people than Finland?
 // The average population of a country is 33 million people. Does you country have less people than the average country?
 // Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
-
 
 myCountryPopulation /= 2;
 console.log(myCountryPopulation);
@@ -59,6 +61,13 @@ let populationOfPortugal = 11;
 let portugalPopulationInMillion = "M";
 let laguageOfPortugal = "portuguese"
 
+console.log(europeanCountry + " is in " + continentOfPortugal + ", " + "and it's " + populationOfPortugal + portugalPopulationInMillion + " people speak " + laguageOfPortugal);
+
+
+//////////////////////////////////////////
+
+// Strings and Template Literals
+// Recreate the description variable from the last assignment, this time using the template literal syntax.
+
+
 console.log(`${europeanCountry} is in ${continentOfPortugal}, and it's ${populationOfPortugal}${portugalPopulationInMillion} people speak ${laguageOfPortugal}`);
-
-
