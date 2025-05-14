0c774be82e7d12ca051684e299a0061ac55fe7f5
hashimmalik59 <hm363050@gmail.com>
2025-05-14 21:07:56 +0500

    Coding Exercise 1 Challenge 1(Operators etc)

---
+++ Javascript/01_section/codingExercise1Challenge1.js
@@ -0,0 +1,17 @@
+// Coding Exercise 1: Challenge#1
+
+// Step 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// Step 2
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// Step 3
+console.log(BMIMark, BMIJohn);
+
+// Step 4
+console.log(BMIMark > BMIJohn);
