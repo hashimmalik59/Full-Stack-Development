83054e8bd7484875659129b79dfe423746c6e502
hashimmalik59 <hm363050@gmail.com>
2025-06-04 10:06:05 +0500

    Intro of Array

---
+++ Javascript/02_section/06_introArray.js
@@ -0,0 +1,12 @@
+// Introduction to Array
+
+// let arr = [1, 2, 3, 4];
+// console.log(arr);
+// arr[1, 2] = ["Apple", "Banana"];
+// let oneArray = arr.flat()
+// console.log(arr);
+// console.log(oneArray);
+
+const fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits);
+console.log(fruits[0])
