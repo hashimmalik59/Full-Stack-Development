d72ca2c4fee70062d62842e9d8d7e37e124a4664
hashimmalik59 <hm363050@gmail.com>
2025-06-04 10:37:55 +0500

    Array basic operator(method)

---
+++ Javascript/02_section/07_arrayBasicOperatorMethod.js
@@ -0,0 +1,9 @@
+// Array basic operator(method)
+
+const fruits = ["Apple", "Banana", "Mango"];
+const newLenght1 = fruits.push("Orange");
+const newLenght2 = fruits.pop();
+const newLenght3 = fruits.unshift("Guava");
+const newLenght4 = fruits.shift();
+console.log(fruits);
+console.log(newLenght1, newLenght2, newLenght3, newLenght4);
