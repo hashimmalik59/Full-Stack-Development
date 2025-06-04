ccf4ae3d3811212a3ed9937bc5ae5fd0a9c75447
hashimmalik59 <hm363050@gmail.com>
2025-06-04 11:31:07 +0500

    Intro to Object

---
+++ Javascript/02_section/08_introObject.js
@@ -0,0 +1,16 @@
+// Intro to Object
+
+const data = [
+    "Hashim",
+    "Apple",
+    23,
+    true
+];
+console.log(data);
+const dataObj = { // Object is a key value pair
+    name: "Hashim",
+    favouriteFruit: "Apple",
+    age: 23,
+    isAdult: true
+}
+console.log(dataObj);
