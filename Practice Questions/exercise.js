981df74151c07335c64365fe00a4faddfb5f32f0
hashimmalik59 <hm363050@gmail.com>
2025-06-04 16:06:01 +0500

    Practice Exercise Easy

---
+++ Practice Questions/exercise.js
@@ -0,0 +1,56 @@
+// ✅ Beginner Level Tasks
+
+// Simple Calculator
+// Take two numbers and an operator (+, -, *, /) as input, perform the operation using if...else.
+
+// const num1 = prompt("Enter your 1st number");
+// const operator = prompt("Enter your Operator");
+// const num2 = prompt("Enter your 2nd number");
+// if (operator === "+") {
+//     console.log(num1 + num2);
+// } else if (operator === "-") {
+//     console.log(num1 - num2);
+// } else if (operator === "*") {
+//     console.log(num1 * num2);
+// } else if (operator === "/") {
+//     console.log(num1 / num2);
+// } else {
+//     console.log("Invalid Operator!");
+// }
+
+// Even or Odd Checker
+// Check if a number is even or odd using the % operator.
+
+// const evenOdd = prompt("Enter your number");
+// if (evenOdd % 2 === 0) {
+//     console.log(`${evenOdd}: is Even number!`);
+// } else {
+//     console.log(`${evenOdd}: is Odd number!`);
+// }
+
+// Array Sum
+// Create an array of numbers and calculate the total sum using a loop.
+
+// const numbers = [1, 2, 3, 4, 5];
+// let n = 0;
+// for (let i = 0; i <= numbers.length; i++) {
+//     n = n + numbers[i]
+//     console.log(n);
+// }
+
+// Object Intro
+// Create an object of a student (name, age, grade) and print all the values.
+
+// const student = {
+//     name: "Hashim",
+//     age: 23,
+//     grade: 705
+// }
+// console.log(student["name"], student["age"], student["grade"]);
+
+// Greeting Function
+// Make a function that takes a name and returns "Hello, [Name]!".
+// function greetName(name) {
+//     return `Hello, ${name}`;
+// }
+// console.log(greetName("Hashim"));
