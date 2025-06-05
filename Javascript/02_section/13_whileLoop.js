1e9169043bfc99f842f3355f8b58c2d073016d89
hashimmalik59 <hm363050@gmail.com>
2025-06-05 18:47:11 +0500

    While Loops

---
+++ Javascript/02_section/13_whileLoop.js
@@ -0,0 +1,13 @@
+// While Loop
+
+// Jab tak condition true hai, code repeat hot rahega
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
