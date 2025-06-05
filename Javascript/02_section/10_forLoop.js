c72c3d27de0c078dd674ef0c01e7d36a3422e6b9
hashimmalik59 <hm363050@gmail.com>
2025-06-05 14:34:05 +0500

    for loop

---
+++ Javascript/02_section/10_forLoop.js
@@ -0,0 +1,20 @@
+// Iterators: The for loop
+
+// console.log(`Pushup 1 🏋️`);
+// console.log(`Pushup 2 🏋️`);
+// console.log(`Pushup 3 🏋️`);
+// console.log(`Pushup 4 🏋️`);
+// console.log(`Pushup 5 🏋️`);
+// console.log(`Pushup 6 🏋️`);
+// console.log(`Pushup 7 🏋️`);
+// console.log(`Pushup 8 🏋️`);
+// console.log(`Pushup 9 🏋️`);
+// console.log(`Pushup 10 🏋️`);
+
+// for loop keeps running while condition is true
+//   initialization  condition  updation
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Pushup ${rep} 🏋️`);
+}
+
+// Note: Not having to repeat the same code over and over again, we use a for loop
